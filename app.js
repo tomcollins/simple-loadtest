2524074b2df7a50533a11451263dd48356153134
tomcollins <tom@tomcollins.co.uk>
2013-07-20 23:41:37 +0100

    Added random cachebuster and configurable interval.

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var numCPUs = os.cpus().length
   , key = argv.cert || 'dev.bbc.co.uk.key'
   , ca = argv.ca || 'ca.pem'
   , tlsOptions
+  , interval = argv.interval || 1000
   , host = argv.host || 'api.travel.test.cloud.bbc.co.uk'
   , path = argv.path || '/travel-incident-api/{id}/incidents'
   , noOfOpenRequests = 0
@@ -31,76 +32,74 @@ var numCPUs = os.cpus().length
   , successCount = 0
   , errorCount = 0;
 
-  tlsOptions = {
-    cert: fs.readFileSync(cert),
-    key: fs.readFileSync(cert),
-    ca: [fs.readFileSync(ca)],
-    host: host,
-    port: 443,
-    path: null,
-    rejectUnauthorized: false,
-    agent: false
-  };
+tlsOptions = {
+  cert: fs.readFileSync(cert),
+  key: fs.readFileSync(cert),
+  ca: [fs.readFileSync(ca)],
+  host: host,
+  port: 443,
+  path: null,
+  rejectUnauthorized: false,
+  agent: false
+};
 
-  inputData = fs.readFileSync(inputFile).toString().split('\n');
-  inputData.forEach(function(line){
-    values = line.split(',');
-    testData.push(values[0]);
-  });
-  testDataLength = testData.length;
+inputData = fs.readFileSync(inputFile).toString().split('\n');
+inputData.forEach(function(line){
+  values = line.split(',');
+  testData.push(values[0]);
+});
+testDataLength = testData.length;
 
-  function doRequest(id) {
-    tlsOptions.path = path.replace('{id}', id);
-    noOfOpenRequests++;
+function doRequest(id) {
+  tlsOptions.path = path.replace('{id}', id) +'?rand=' +Math.floor(Math.random() * 999999999);;
+  noOfOpenRequests++;
 
-    var req = https.get(tlsOptions, function(res) {
-      //console.log('Status: ' + res.statusCode);
-      //console.log('Headers: ' + JSON.stringify(res.headers));
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
-        //console.log('BODY: ' + chunk);
-      });
-      res.on('end', function (chunk) {
-        if (200 == res.statusCode) successCount++;
-        else errorCount++;
-        noOfOpenRequests--;
-        requestsSinceUpdate++;
-        req = null;
-        res = null;
-      });
+  var req = https.get(tlsOptions, function(res) {
+    //console.log('Status: ' + res.statusCode);
+    //console.log('Headers: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      //console.log('BODY: ' + chunk);
     });
-    req.on('error', function(e) {
-      //console.log('problem with request: ' + e.message);
-      errorCount++;
+    res.on('end', function (chunk) {
+      if (200 == res.statusCode) successCount++;
+      else errorCount++;
       noOfOpenRequests--;
       requestsSinceUpdate++;
       req = null;
       res = null;
     });
-    req.end();
+  });
+  req.on('error', function(e) {
+    //console.log('problem with request: ' + e.message);
+    errorCount++;
+    noOfOpenRequests--;
+    requestsSinceUpdate++;
+    req = null;
+    res = null;
+  });
+  req.end();
+}
+
+function nextRequest() {
+  if (noOfOpenRequests < maxOpenRequests) {
+    doRequest(testData[testDataIndex++]);
+    if (testDataIndex >= testDataLength) testDataIndex = 0;
   }
-
-  function nextRequest() {
-    if (noOfOpenRequests < maxOpenRequests) {
-      doRequest(testData[testDataIndex++]);
-      if (testDataIndex >= testDataLength) testDataIndex = 0;
-    }
-  };
-  nextRequest();
-  setInterval(nextRequest, 10);
+};
+nextRequest();
+setInterval(nextRequest, interval);
 
 
-  console.log('Max concurrent requests', maxOpenRequests, ' (use ulimit -n $ to increase).');
-  console.log('Using', testDataLength, 'location ids.')
+console.log('Max concurrent requests', maxOpenRequests, ' (use ulimit -n $ to increase).');
+console.log('Using', testDataLength, 'location ids.')
 
-  function update() {
-    time = new Date().getTime();
-    elapsedTime = time - updateTime;
-    updateTime = time;
-    console.log('Update: ' +Number(requestsSinceUpdate/(elapsedTime/1000)).toFixed(1) +'/s, C=' +successCount +', E=' +errorCount +', O: ' +noOfOpenRequests +'/' +maxOpenRequests +')');
-    requestsSinceUpdate = 0;
-  };
-  updateTime = new Date().getTime();
-  setInterval(update, 1000);
-
-
+function update() {
+  time = new Date().getTime();
+  elapsedTime = time - updateTime;
+  updateTime = time;
+  console.log('Update: ' +Number(requestsSinceUpdate/(elapsedTime/1000)).toFixed(1) +'/s, C=' +successCount +', E=' +errorCount +', O: ' +noOfOpenRequests +'/' +maxOpenRequests +')');
+  requestsSinceUpdate = 0;
+};
+updateTime = new Date().getTime();
+setInterval(update, 1000);
